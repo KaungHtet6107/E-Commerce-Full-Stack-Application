a6f203bb8e5ce6395e21f6c30f5b923184c7a100
Kaung Htet <kaunghtet061097@gmail.com>
2026-05-18 18:47:27 +0630

    Update ProductService.java

--- backend/src/main/java/com/ecommerce/project/service/ProductService.java
+++ backend/src/main/java/com/ecommerce/project/service/ProductService.java
@@ -19,7 +19,7 @@ public interface ProductService {
 
     ProductDTO deleteProduct(Long productId);
 
-    ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
+    ProductDTO updateProductImage(Long productId, String image);
 
     ProductResponse getAllProductsForAdmin(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
