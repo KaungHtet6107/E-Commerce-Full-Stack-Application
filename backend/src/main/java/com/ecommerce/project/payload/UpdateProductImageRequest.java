10dbb1a64553f55f7ad90487c6be856acaabfc87
Kaung Htet <kaunghtet061097@gmail.com>
2026-05-18 17:51:15 +0630

    Create UpdateProductImageRequest.java

---
+++ backend/src/main/java/com/ecommerce/project/payload/UpdateProductImageRequest.java
@@ -0,0 +1,14 @@
+package com.ecommerce.project.payload;
+
+public class UpdateProductImageRequest {
+
+    private String imageUrl;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
