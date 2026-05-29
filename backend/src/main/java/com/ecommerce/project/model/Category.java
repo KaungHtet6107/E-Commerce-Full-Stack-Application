4301ed209a6be64ce4379542d9cb86907d641dea
Kaung Htet <kaunghtet061097@gmail.com>
2026-05-29 18:30:00 +0630

    Add JsonIgnore annotation to products list

--- backend/src/main/java/com/ecommerce/project/model/Category.java
+++ backend/src/main/java/com/ecommerce/project/model/Category.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,5 +24,6 @@ public class Category {
     private String categoryName;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    @JsonIgnore
     private List<Product> products;
 }
