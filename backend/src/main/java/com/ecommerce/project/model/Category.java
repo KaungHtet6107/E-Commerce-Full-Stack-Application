83aec23120c80a7073ec96211a4a98d9e780efb7
Kaung Htet <kaunghtet061097@gmail.com>
2026-05-29 21:22:18 +0630

    Update Category.java

--- backend/src/main/java/com/ecommerce/project/model/Category.java
+++ backend/src/main/java/com/ecommerce/project/model/Category.java
@@ -18,9 +18,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
-
-    @NotBlank
-    @Size(min = 5, message = "Category name must contain atleast 5 characters")
+    
     private String categoryName;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
